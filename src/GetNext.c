65f6d61824345cf54dde66251133b4d1a8334bcf
ripter <ripter001@gmail.com>
2024-09-21 19:35:06 -0400

    All Tests Pass!

--- src/GetNext.c
+++ src/GetNext.c
@@ -9,37 +9,29 @@
 // Returns the next word from the input stream
 char* GetNext(FILE* input) {
   char wordBuffer[MAX_WORD_LENGTH]; 
-  int wordStart = 0;
-  int wordStop = 0;
+  int wordLength = 0;
   char c;
 
   // Loop until we reach the end of the file or a whitespace character
-  while ( (c = fgetc(input)) ) {
+  while ((c = fgetc(input)) ) {
     // Check for both types of EOF, break or error.
-    if ((c == EOF) && feof(input)) { break; } // End of file reached, handle like like whitespace.
+    if ((c == EOF) && feof(input)) { break; } // End of file reached, handle like like end of word.
     if ((c == EOF) && ferror(input)) { fprintf(stderr, "\nError: reading from file\n"); return NULL; }
     // ignore leading whitespace, bail on trailing whitespace.
-    if (IsWhitespace(c)) {
-      if (wordStart == wordStop) { 
-        wordStart += 1;
-        wordStop += 1;
-        continue;
-      }
-      else { break; }
-    }
-    // Error if there are still characters in the stream after we reach the max word length.
-    if ( wordStop >= (MAX_WORD_LENGTH) ) { fprintf(stderr, "\nError: Word too long to process\n"); return NULL; }
-    wordBuffer[wordStop] = c;
-    wordStop += 1;
+    if (IsWhitespace(c) && wordLength == 0) { continue; }
+    if (IsWhitespace(c) && wordLength > 0) { break; }
+    // Make sure we don't exceed the max word length
+    if (wordLength >= (MAX_WORD_LENGTH) ) { fprintf(stderr, "\nError: Word too long to process\n"); return NULL; }
+    wordBuffer[wordLength] = c;
+    wordLength += 1;
   }
 
-  if (wordStop == 0) {
+  if (wordLength == 0) {
     fprintf(stderr, "\nError: reading from file. Unable to reach any characters.\n");
     return NULL;
   }
 
   // Allocate a new string to hold the word, including the \0  
-  int wordLength = wordStop - wordStart;
   char* word = MemAlloc(sizeof(char) * (wordLength+1));
   if (word == NULL) {
     fprintf(stderr, "\nError: Memory allocation failed\n");
