f0b42bef192cad2762f81d00864757fefbb3f82e
ripter <ripter001@gmail.com>
2024-09-25 18:44:24 -0400

    formatting.

--- src/core/CoreWords.c
+++ src/core/CoreWords.c
@@ -7,12 +7,12 @@
 // Adds the Core Words to the dictionary.
 // Words *MUST* be lowercase to be found.
 void AddCoreWords(KernelState *state) {
-  AddItemToDictionary(&state->dict, "+",        (WordMetadata){(xt_func_ptr)Add,        false, NULL}); 
-  AddItemToDictionary(&state->dict, "-",        (WordMetadata){(xt_func_ptr)Subtract,   false, NULL});
-  AddItemToDictionary(&state->dict, ".",        (WordMetadata){(xt_func_ptr)Dot,        false, NULL});
-  AddItemToDictionary(&state->dict, "branch",   (WordMetadata){(xt_func_ptr)Branch,     false, NULL});
-  AddItemToDictionary(&state->dict, "branch?",  (WordMetadata){(xt_func_ptr)BranchZ,    false, NULL});
-  AddItemToDictionary(&state->dict, ".s",       (WordMetadata){(xt_func_ptr)DotS,       false, NULL});
-  AddItemToDictionary(&state->dict, ":",        (WordMetadata){(xt_func_ptr)Colon,      false, NULL});
-  AddItemToDictionary(&state->dict, ";",        (WordMetadata){(xt_func_ptr)SemiColon,  false, NULL});
+  AddItemToDictionary(&state->dict, "+",        (WordMetadata){(xt_func_ptr)Add,        false,  NULL}); 
+  AddItemToDictionary(&state->dict, "-",        (WordMetadata){(xt_func_ptr)Subtract,   false,  NULL});
+  AddItemToDictionary(&state->dict, ".",        (WordMetadata){(xt_func_ptr)Dot,        false,  NULL});
+  AddItemToDictionary(&state->dict, "branch",   (WordMetadata){(xt_func_ptr)Branch,     false,  NULL});
+  AddItemToDictionary(&state->dict, "branch?",  (WordMetadata){(xt_func_ptr)BranchZ,    false,  NULL});
+  AddItemToDictionary(&state->dict, ".s",       (WordMetadata){(xt_func_ptr)DotS,       false,  NULL});
+  AddItemToDictionary(&state->dict, ":",        (WordMetadata){(xt_func_ptr)Colon,      false,  NULL});
+  AddItemToDictionary(&state->dict, ";",        (WordMetadata){(xt_func_ptr)SemiColon,  true,   NULL});
 }
